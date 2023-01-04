023e3760613d5b389f6a3662d6fb21043449569a
mxy <13614169547@163.com>
2023-01-04 14:22:45 +0800

    修正点赞Bug

--- src/main/java/com/mxy/bbs_server/controller/ActionController.java
+++ src/main/java/com/mxy/bbs_server/controller/ActionController.java
@@ -4,6 +4,7 @@ import com.mxy.bbs_server.entity.ActionRequest;
 import com.mxy.bbs_server.response.action.ActionResponse;
 import com.mxy.bbs_server.service.ActionService;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,12 +14,12 @@ public class ActionController {
 
     private final ActionService actionService;
 
-    public ActionController(ActionService actionService) {
+    public ActionController(@RequestBody ActionService actionService) {
         this.actionService = actionService;
     }
 
     @PostMapping("/like")
-    public ActionResponse like(ActionRequest actionRequest) {
+    public ActionResponse like(@RequestBody ActionRequest actionRequest) {
         return actionService.like(actionRequest);
     }
 }
